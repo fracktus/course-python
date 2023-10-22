c0cdeefb889c0fb8c39c4635a31791dd837c9a02
polyanastasia <73426393+fracktus@users.noreply.github.com>
2023-10-22 08:38:46 +0300

    Add files via upload

---
+++ лаба2 цк/task2.py
@@ -0,0 +1,14 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+money_capital = 0
+capitals = []
+
+for i in range(1, months+1):
+    money_capital = spend - salary
+    spend = spend + spend * increase
+    capitals.append(money_capital)
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(sum(capitals), 2))
